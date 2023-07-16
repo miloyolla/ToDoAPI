01b789cac28b2bdc6b5ccbfc95c9fdbbcdd8a458
Mileny Loyolla <mileny.loyolla@gmail.com>
2023-07-16 16:06:01 -0300

    add Model e Migration de Tarefa

---
+++ ToDo.Domain/Models/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public int StatusId { get; private set; }
+
+        public Tarefa(string nome, int statusId)
+        {
+            Nome = nome;
+            StatusId = statusId;
+        }
+    }
+}
---
+++ ToDo.Infra/Mappings/TarefaMap.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Domain.Models;
+
+namespace ToDo.Infra.Mappings
+{
+    public class TarefaMap : IEntityTypeConfiguration<Tarefa>
+    {
+        public void Configure(EntityTypeBuilder<Tarefa> builder)
+        {
+            builder.Property(t => t.Id)
+                .HasColumnName("Id")
+                .UseIdentityColumn();
+
+            builder.HasKey("Id");
+
+            builder.Property(t => t.Nome)
+                .HasColumnType("nvarchar(50)")
+                .IsRequired();
+
+            builder.Property(t => t.StatusId)
+                .HasColumnType("int")
+                .IsRequired();
+        }
+    }
+}
